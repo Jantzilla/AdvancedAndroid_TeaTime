8f5e473de720ce861e10b228499a4eea175e56bb
Jantz <jantzillac.93@gmail.com>
2018-05-20 19:26:41 -0500

    Finish this method which verifies that the intent sent by clicking the send email button matches the intent sent by the application

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -18,14 +18,22 @@ package com.example.android.teatime;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Intent;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 // COMPLETED (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
 @RunWith(AndroidJUnit4.class)
 public class OrderSummaryActivityTest {
+
+    private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
 
     // COMPLETED (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
     @Rule
@@ -43,10 +51,18 @@ public class OrderSummaryActivityTest {
     }
 
 
-    // TODO (4) Finish this method which verifies that the intent sent by clicking the send email
+    // COMPLETED (4) Finish this method which verifies that the intent sent by clicking the send email
     // button matches the intent sent by the application
 
+    @Test
     public void clickSendEmailButton_SendsEmail() {
+
+        onView(withId(R.id.send_email_button)).perform(click());
+        // intended(Matcher<Intent> matcher) asserts the given matcher matches one and only one
+        // intent sent by the application.
+        intended(allOf(
+                hasAction(Intent.ACTION_SENDTO),
+                hasExtra(Intent.EXTRA_TEXT, emailMessage)));
 
     }
 }
